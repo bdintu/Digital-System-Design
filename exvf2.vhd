d35c6a2957c2cdb96630b1bc9344292a242f66fe
bdintu <4646555+bdintu@users.noreply.github.com>
2018-01-30 14:28:37 +0700

    lab01-webpack

---
+++ exvf2.vhd
@@ -0,0 +1,17 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.STD_LOGIC_UNSIGNED.ALL;
+
+entity exvf2 is
+	Port(	A, B	: in  STD_LOGIC_VECTOR (2 downto 0);
+			C		: out  STD_LOGIC_VECTOR (3 downto 0)
+	);
+end exvf2;
+
+architecture Behavioral of exvf2 is
+
+begin
+
+	C <= ('0'&A) + ('0'&B);
+
+end Behavioral;
