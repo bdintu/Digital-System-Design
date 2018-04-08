34118a5e51e5b85798f70b07ef595f6049004f26
j <j@j.j>
2018-04-09 03:43:27 +0700

    Lab7_BasicALU: add mux

--- Lab7_BasicALU/BasicALU.vhd
+++ Lab7_BasicALU/BasicALU.vhd
@@ -14,30 +14,23 @@ end BasicALU;
 
 architecture Behavioral of BasicALU is
 	signal notA	: STD_LOGIC_VECTOR (7 downto 0);
-	signal add	: STD_LOGIC_VECTOR (7 downto 0);
-	signal sub	: STD_LOGIC_VECTOR (7 downto 0);
-	signal xoor	: STD_LOGIC_VECTOR (7 downto 0);
-	signal shl	: STD_LOGIC_VECTOR (7 downto 0);
- 
-	signal X	: STD_LOGIC_VECTOR (3 downto 0);
-	signal ANS	: STD_LOGIC_VECTOR (7 downto 0);
+	signal notB	: STD_LOGIC_VECTOR (7 downto 0);
+	signal ANS	: STD_LOGIC_VECTOR (7 downto 0) := "00000000";
+	signal X		: STD_LOGIC_VECTOR (3 downto 0);
 	signal fre_count	: integer range (0) to (100000) := 0;
 
 begin
 
 	notA	<= (not A);
-	add		<= (not A) + (not B);
-	sub		<= (not A) - (not B);
-	xoor	<= (not A) xor (not B);
-	shl		<= notA(6 downto 0) & '0';
+	notB	<= (not B);
 
-	ANS		<=	add when SW(0) = '1' else 
-				sub when SW(1) = '1' else
-				xoor when SW(2) = '1' else
-				shl when SW(3) = '1';
+	ANS	<=	notA + notB		when SW = "0001" else		-- ADD
+				notA - notB 	when SW = "0010" else		-- SUB
+				notA xor notB	when SW = "0100" else		-- XOR
+				notA(6 downto 0) & '0' when SW = "1000";	-- SHL
 
-	process(clk) is
-		begin
+	process (clk)
+	begin	
 
 		if (rising_edge(clk)) then
 			fre_count <= fre_count  + 1;
@@ -52,21 +45,21 @@ begin
 
 	end process;
 
-	C <="1110001" when X = "1111" else -- F
-		"1111001" when X = "1110" else -- E
-		"1011110" when X = "1101" else -- D
-		"0111001" when X = "1100" else -- C
-		"1111100" when X = "1011" else -- B
-		"1110111" when X = "1010" else -- A
-		"1101111" when X = "1001" else -- 9
-		"1111111" when X = "1000" else -- 8
-		"0000111" when X = "0111" else -- 7
-		"1111101" when X = "0110" else -- 6
-		"1101101" when X = "0101" else -- 5
-		"1100110" when X = "0100" else -- 4
-		"1001111" when X = "0011" else -- 3
-		"1011011" when X = "0010" else -- 2
-		"0000110" when X = "0001" else -- 1
-		"0111111";
+	C	<=	"1110001" when X = "1111" else -- F
+			"1111001" when X = "1110" else -- E
+			"1011110" when X = "1101" else -- D
+			"0111001" when X = "1100" else -- C
+			"1111100" when X = "1011" else -- B
+			"1110111" when X = "1010" else -- A
+			"1101111" when X = "1001" else -- 9
+			"1111111" when X = "1000" else -- 8
+			"0000111" when X = "0111" else -- 7
+			"1111101" when X = "0110" else -- 6
+			"1101101" when X = "0101" else -- 5
+			"1100110" when X = "0100" else -- 4
+			"1001111" when X = "0011" else -- 3
+			"1011011" when X = "0010" else -- 2
+			"0000110" when X = "0001" else -- 1
+			"0111111";
 
 end Behavioral;
